cb2219723cc0e333649238b82a15bb30c4801f56
yudegaki <42182478+yudegaki@users.noreply.github.com>
2019-11-26 21:50:29 +0900

    Add files via upload

---
+++ block_samurai/add-func.js
@@ -0,0 +1,22 @@
+
+let el = document.getElementsByClassName('g'),
+    pattern = 'www.sejuku.net/',
+    ad;
+if(document.querySelector("#vn1s0p1c0 > div > cite") != null){
+    ad = document.querySelector("#vn1s0p1c0 > div > cite");
+    if(ad.textContent == null);
+    else if(ad.textContent == pattern){
+        let rm = document.querySelector('#taw');
+        rm.textContent = null;
+    }
+}
+for(let item of el){
+    let h,result;
+    if(item.querySelector('div > div.rc > div.r > a') == null)continue;
+    h = item.querySelector('div > div.rc > div.r > a');
+    if(h.href == null)continue;
+    result = h.href.match(pattern);
+    if(result != null){
+        item.textContent = null;
+    }
+}
